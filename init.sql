e7513694262cdd8c264763c72dd051947437bd38
Jeremy Lehman <jeremylehman9@gmail.com>
2017-03-02 15:11:20 +0000

    actually add init.sql

---
+++ init.sql
@@ -0,0 +1,16 @@
+DROP TABLE Machine;
+CREATE TABLE Machine (
+  Name varchar NOT NULL,
+  Dorm varchar NOT NULL,
+  PRIMARY KEY (Name, Dorm)
+);
+
+CREATE TABLE MachineStateLog (
+  MachineName varchar NOT NULL,
+  MachineDorm varchar NOT NULL,
+  StartTime DATE,
+  State varchar NOT NULL,
+  TimeLeft NUMERIC DEFAULT 0, 
+  FOREIGN KEY (MachineName, MachineDorm) REFERENCES Machine(Name, Dorm),
+  PRIMARY KEY (MachineName, MachineDorm, StartTime)
+);
