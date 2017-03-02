abba03d6767583a2c7870b1780654379030e7fbc
Jeremy Lehman <jeremylehman9@gmail.com>
2017-03-02 15:38:29 +0000

    ammend drop statment for machinestatlog

--- init.sql
+++ init.sql
@@ -5,6 +5,7 @@ CREATE TABLE Machine (
   PRIMARY KEY (Name, Dorm)
 );
 
+DROP TABLE IF EXISTS MachineStateLog;
 CREATE TABLE MachineStateLog (
   MachineName varchar NOT NULL,
   MachineDorm varchar NOT NULL,
